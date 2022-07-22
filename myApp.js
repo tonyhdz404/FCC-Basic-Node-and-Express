46c79ce6b48c4ebf4a4c8bcef8ef8e9c635e414f
tonyhdz404 <antonio.hernandez030@gmail.com>
2022-07-22 10:23:53 -0600

    Added json route

--- myApp.js
+++ myApp.js
@@ -6,4 +6,11 @@ app.use("/public", express.static(`${__dirname}/public`));
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
+
 module.exports = app;
