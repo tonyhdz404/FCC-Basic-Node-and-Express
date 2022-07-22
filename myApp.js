45cec2f2b2ad9bad163a07d2aadaa2d33ca9c6e3
tonyhdz404 <antonio.hernandez030@gmail.com>
2022-07-22 10:16:19 -0600

    Added static folder access

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
-console.log("Hello Worldz");
+
+app.use(`${__dirname}/public`,express.static())
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
