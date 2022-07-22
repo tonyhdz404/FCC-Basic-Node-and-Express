1935b9eee7cde422c729e623b03022e8c8581559
tonyhdz404 <antonio.hernandez030@gmail.com>
2022-07-22 10:28:27 -0600

    commit

--- myApp.js
+++ myApp.js
@@ -8,9 +8,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
+  if ((process.env.MESSAGE_STYLE = "uppercase")) {
+    res.json({
+      message: "HELLO JSON",
+    });
+  } else {
+    res.json({
+      message: "Hello json",
+    });
+  }
 });
 
 module.exports = app;
